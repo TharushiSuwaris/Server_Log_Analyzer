20750b0f9a2be2f162862cd9537814d6ab3c2c95
Tharushi Suwaris <tharushi1995619@gmail.com>
2022-05-20 20:38:00 +0530

    remove digits

--- PreProcess.java
+++ PreProcess.java
@@ -8,9 +8,14 @@ import java.io.FileWriter;
 import java.util.Set;
 
 public class PreProcess{
+
+    public String removeDigits(String line){
+        String result = line.replaceAll("\\(\\d+\\)", "");
+        result = result.replaceAll("\\[\\d+\\]", "");
+        return result;
+    }
     
     public void processLines(String fileName) throws IOException {
-        
         //read log file and write
         try{
             File logFile = new File(fileName);
@@ -20,8 +25,8 @@ public class PreProcess{
             Set set = new HashSet();
             while(reader.hasNextLine()){
                 String line = reader.nextLine();
-                //int index = line.indexOf("]");
                 line = line.substring(25);
+                line = removeDigits(line);
                 if(set.add(line)){
                     out.write(line);
                     out.newLine();
