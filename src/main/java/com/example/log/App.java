4c4115cfbfa41afbe9b2a50f32f2eba36677a9d8
Tharushi Suwaris <tharushi1995619@gmail.com>
2022-05-22 23:00:13 +0530

    find errors and cause

--- src/main/java/com/example/log/App.java
+++ src/main/java/com/example/log/App.java
@@ -2,22 +2,30 @@ package com.example.log;
 
 import java.io.IOException;
 
+import org.json.simple.JSONObject;
+
 public class App 
 {
+    public static String extractErrors(JSONObject obj) {
+        String key ="";
+        for (Object keyStr : obj.keySet()) {
+            //Object keyvalue = obj.get(keyStr);
+            key = keyStr.toString();
+            System.out.println(key);
+            return key;
+        }
+        return key;
+    } 
     public static void main(String[] args) throws IOException {
         PreProcess process = new PreProcess();
         process.processLines("mz.0");
-        process.readJson();
-        /*try{
-            File logFile = new File("ew.txt");
-            Scanner reader = new Scanner(logFile);
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-                process.checker("newFile.txt",line );
-            }
-            reader.close();
-        }catch(FileNotFoundException e){
-            System.out.println("An error occurred while reading.");
-        }*/
+        JSONObject array =  process.readJson();
+        //String key = extractErrors(array);
+        for (Object keyStr : array.keySet()) {
+            Object keyvalue = array.get(keyStr);
+            String key = keyStr.toString();
+            String keyv = keyvalue.toString();
+            process.checker("newFile.txt", key, keyv);
+        }
     }
 }
--- src/main/java/com/example/log/PreProcess.java
+++ src/main/java/com/example/log/PreProcess.java
@@ -5,13 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.Set;
 
 import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -48,13 +49,12 @@ public class PreProcess{
             System.out.println("An error occurred while reading.");
         }
     }
-    public void readJson() {
+    public JSONObject readJson() {
+        JSONObject object = new JSONObject();;
         JSONParser jsonParser = new JSONParser();
         try{
             FileReader reader = new FileReader("errors.json");
-            Object obj = jsonParser.parse(reader);
-            JSONArray list = (JSONArray) obj;
-            System.out.print(list);
+            object = (JSONObject) jsonParser.parse(reader);
         }catch(FileNotFoundException e){
             System.out.println("An error occurred while reading.");
         }catch (IOException e) {
@@ -62,16 +62,17 @@ public class PreProcess{
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        return object;
     }
     
-    public void checker(String fileName, String eLine) {
+    public void checker(String fileName, String eLine, String value) {
         try{
             File logFile = new File(fileName);
             Scanner reader = new Scanner(logFile);
             while(reader.hasNextLine()){
                 String line = reader.nextLine();
                 if(line.matches(eLine)){
-                    System.out.println("found");
+                    System.out.println("found :"+eLine+"--------"+value);
                 }
             }
             reader.close();
